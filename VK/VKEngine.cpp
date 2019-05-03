44c16e6f9f25592bdecabc5cf120a1d1031656d6
Unknown <cedricschwyter@bluewin.ch>
2019-05-03 13:36:57 +0200

    Hello Triangle!

--- VK/VKEngine.cpp
+++ VK/VKEngine.cpp
@@ -902,7 +902,7 @@ VK_STATUS_CODE VKEngine::createGraphicsPipelines() {
 	rasterizationStateCreateInfo.polygonMode									= VK_POLYGON_MODE_FILL;
 	rasterizationStateCreateInfo.lineWidth										= 1.0f;
 	rasterizationStateCreateInfo.cullMode										= VK_CULL_MODE_BACK_BIT;
-	rasterizationStateCreateInfo.frontFace										= VK_FRONT_FACE_COUNTER_CLOCKWISE;
+	rasterizationStateCreateInfo.frontFace										= VK_FRONT_FACE_CLOCKWISE;
 	rasterizationStateCreateInfo.depthBiasEnable								= VK_FALSE;
 
 	// No multisampling (yet)
