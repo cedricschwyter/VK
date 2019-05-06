47dfa31d7d960cc42efb52402a9cf19cd87616ce
Unknown <cedricschwyter@bluewin.ch>
2019-05-06 13:46:49 +0200

    Initialize vert/frag shader stage info to null

--- VK/VertFragShaderStages.cpp
+++ VK/VertFragShaderStages.cpp
@@ -24,11 +24,13 @@ VertFragShaderStages::VertFragShaderStages(const char* vertPath_, const char* fr
 	vertModule											= createShaderModuleFromBinary(&vertCode);
 	fragModule											= createShaderModuleFromBinary(&fragCode);
 
+	vertStageInfo										= {};
 	vertStageInfo.sType									= VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
 	vertStageInfo.stage									= VK_SHADER_STAGE_VERTEX_BIT;
 	vertStageInfo.module								= vertModule;
 	vertStageInfo.pName									= "main";
 
+	fragStageInfo										= {};
 	fragStageInfo.sType									= VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
 	fragStageInfo.stage									= VK_SHADER_STAGE_FRAGMENT_BIT;
 	fragStageInfo.module								= fragModule;
