c4aa38700fb465a479c1d4775afb600b64997ebb
Unknown <cedricschwyter@bluewin.ch>
2019-05-12 12:50:48 +0200

    Add dropped file

---
+++ VK/IndexBuffer.cpp
@@ -0,0 +1,11 @@
+/**
+    Implements the IndexBuffer class, inheriting BaseBuffer
+
+    @author		D3PSI
+    @version	0.0.1 02.12.2019
+
+    @file		IndexBuffer.cpp
+    @brief		Implementation of the IndexBuffer class
+*/
+#include "IndexBuffer.hpp"
+
