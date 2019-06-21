a1f5ddc82ec273dcd9107993acd1dab022d789a5
Unknown <cedricschwyter@bluewin.ch>
2019-06-21 20:54:24 +0200

    Add basic model-loading stuff, no implementation yet

--- VK/Model.cpp
+++ VK/Model.cpp
@@ -8,3 +8,41 @@
     @brief        Implementation of the Model class
 */
 #include "Model.hpp"
+#include "VK.hpp"
+
+
+Model::Model(const char* path_, GraphicsPipeline& pipeline_) : pipeline(pipeline_) {
+
+    loadOBJ(path_);
+
+}
+
+void Model::bind() {
+
+
+
+}
+
+VK_STATUS_CODE Model::loadOBJ(const char* path_) {
+
+
+
+    return vk::errorCodeBuffer;
+
+}
+
+
+void Model::processASSIMPNode(aiNode* node_, const aiScene* scene_) {
+
+    
+
+}
+
+
+Mesh Model::processASSIMPMesh(aiMesh* mesh_, const aiScene* scene_) {
+
+
+
+    return Mesh(nullptr, nullptr, nullptr);
+
+}
--- VK/Model.hpp
+++ VK/Model.hpp
@@ -8,15 +8,63 @@
     @brief        Declaration of the Model class
 */
 #pragma once
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include "GraphicsPipeline.hpp"
+#include "Mesh.hpp"
+
 class Model
 {
 public:
 
+    GraphicsPipeline pipeline;
 
+    /**
+        Constructor
+
+        @param      path_           The path to the .obj-file
+        @param      pipeline_       The pipeline to render the model with
+    */
+    Model(const char* path_, GraphicsPipeline& pipeline_);
+
+    /**
+        Binds the model and the correct uniforms
+    */
+    void bind(void);
 
 private:
 
+    std::vector< Mesh > meshes;
+    std::string directory;
 
+    /**
+        Handles and coordinates all loading actions for the specified file
+
+        @param      path_       The path to the .obj-file to load
+
+        @return     Returns VK_SC_SUCCESS on success
+    */
+    VK_STATUS_CODE loadOBJ(const char* path_);
+
+    /**
+        Helper function for ASSIMP's node-loading system
+
+        @param      node_       A pointer to ASSIMP's node
+        @param      scene_      A pointer to ASSIMP's scene
+    */
+    void processASSIMPNode(aiNode* node_, const aiScene* scene_);
+
+    /**
+        Helper function for ASSIMP's mesh-loading system
+
+        @param      mesh_       A pointer to ASSIMP's mesh
+        @param      scene_      A pointer to ASSIMP's scene
+
+        @return     Returns a (parsed) Mesh-object that VKEngine can work with
+    */
+    Mesh processASSIMPMesh(aiMesh* mesh_, const aiScene* scene_);
 
 };
 
