4fbd7a1d26ea040ce356e15c0edfb41392fbef09
Unknown <cedricschwyter@bluewin.ch>
2019-07-29 17:01:30 +0200

    Still weird texturing error

--- VK/shaders/standard/compile.bat
+++ VK/shaders/standard/compile.bat
@@ -1,10 +1,10 @@
 ::
 ::      Implements batch-commands to compile GLSL-files to SPIR-V format
 ::
-::      @author        D3PSI
-::      @version        0.0.1 02.12.2019
+::      @author       D3PSI
+::      @version      0.0.1 02.12.2019
 ::
-::      @file            compile.bat
+::      @file         compile.bat
 ::      @brief        Implementation of batch-commands to compile GLSL-files to SPIR-V format
 ::
 
