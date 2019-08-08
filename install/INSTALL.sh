91b181b461b7527bf46e47a3333d394ee32dd44d
D3PSI <d3psigames@gmail.com>
2019-08-08 11:17:48 +0200

    Edit install/INSTALL.sh

--- install/INSTALL.sh
+++ install/INSTALL.sh
@@ -4,6 +4,6 @@ echo "Installing dependencies..."
 
 sudo apt update
 
-sudo apt install g++ make gcc cmake xorg-dev libglu1-mesa-dev libglfw3-dev libassimp-dev assimp-utils libglm-dev libsdl2-dev libsdl2-2.0-0 libjpeg9-dev libwebp-dev libtiff5-dev libsdl2-image-dev libsdl2-image-2.0-0 libvulkan1 mesa-vulkan-drivers vulkan-utils -y
+sudo apt install g++ make gcc cmake xorg-dev libglu1-mesa-dev libglfw3-dev libassimp-dev assimp-utils libglm-dev libsdl2-dev libsdl2-2.0-0 libjpeg-dev libwebp-dev libtiff5-dev libsdl2-image-dev libsdl2-image-2.0-0 libvulkan1 mesa-vulkan-drivers vulkan-utils -y
 
 echo "Successfully installed dependencies"
