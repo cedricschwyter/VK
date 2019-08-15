7d7b72f5a2d0e0bf3c22e13ab59d8b130fbd8420
D3PSI <d3psigames@gmail.com>
2019-08-15 14:37:24 +0200

    Edit RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Copying executable to executable directory..."
+echo "Copying executable to runtime directory..."
 cp "bin/Linux/x64/VK by D3PSI" "VK/VK by D3PSI"
 echo "Running VK..."
 function code { cd VK/; }
