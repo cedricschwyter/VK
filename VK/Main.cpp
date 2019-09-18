ba17f2d235c9fbd369e4d8a938387b514115b3da
D3PSI <d3psigames@gmail.com>
2019-09-18 08:19:25 +0200

    Start to implement double pendulum's physics

--- VK/Main.cpp
+++ VK/Main.cpp
@@ -9,41 +9,78 @@
 */
 #include "VK.hpp"
 
-/**
-    Returns the model matrix for the rose model  
-  
-    @return     Returns a glm::mat4
-*/
-glm::mat4 rose() {
+namespace dp {
 
-    static auto                     start           = std::chrono::high_resolution_clock::now();
-    auto                            current         = std::chrono::high_resolution_clock::now();
-                                
-    float                           delta           = std::chrono::duration< float, std::chrono::seconds::period >(current - start).count();        // Namespaces are a fricking mess in <chrono>
-            
-    glm::mat4 model;
-    model           = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.05f, 0.0f));
-    model           = glm::scale(model, glm::vec3(0.005f));
-    model           = glm::rotate(model, delta * glm::radians(90.0f), glm::vec3(0.0f, 0.05f, 0.0f));
-    model[1][1]     *= -1.0f;
+    const double        PI                      = 3.1415;
 
-    return model;
+    uint32_t            p1_length               = 1;
+    uint32_t            p2_length               = 1;
+    float               p1_theta                = PI / 4;
+    float               p2_theta                = PI / 4;
+    float               p1_vel                  = 0.1;
+    float               p2_vel                  = 0.1;
+    float               p1_acc                  = 0;
+    float               p2_acc                  = 0;
+    glm::vec3           p1_origin               = ORIGIN;
+    glm::vec3           p1_pos                  = glm::vec3(p1_length * glm::cos(p1_theta), 0, p1_length * glm::sin(p1_theta));
+    glm::vec3           p2_origin               = p1_pos;
+    glm::vec3           p2_pos                  = glm::vec3(p2_length * glm::cos(p2_theta), 0, p2_length * glm::sin(p2_theta));
+    uint32_t            p1_mass                 = 5;
+    uint32_t            p2_mass                 = 5;
+    float               delta_p1_vel            = -0.000001;
+    float               delta_p2_vel            = -0.000001;
 
-}
+    /**
+        Returns the model matrix for the first stick model
 
-/**
-    Returns the model matrix for the floating_city model  
-  
-    @return     Returns a glm::mat4
-*/
-glm::mat4 city() {
+        @return     Returns a glm::mat4
+    */
+    glm::mat4 stick1() {
 
-    glm::mat4 model;
-    model           = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
-    model           = glm::scale(model, glm::vec3(0.02f));
-    model[1][1]     *= -1.0f;
+        glm::mat4 model;
 
-    return model;
+        return model;
+
+    }
+
+    /**
+        Returns the model matrix for the second stick model
+
+        @return     Returns a glm::mat4
+    */
+    glm::mat4 stick2() {
+
+        glm::mat4 model;
+
+        return model;
+
+    }
+
+    /**
+        Returns the model matrix for the first ball model
+
+        @return     Returns a glm::mat4
+    */
+    glm::mat4 ball1() {
+
+        glm::mat4 model;
+
+        return model;
+
+    }
+
+    /**
+        Returns the model matrix for the second ball model
+
+        @return     Returns a glm::mat4
+    */
+    glm::mat4 ball2() {
+
+        glm::mat4 model;
+
+        return model;
+
+    }
 
 }
 
@@ -54,8 +91,10 @@ int main() {
 
     vk::init();
 
-    vk::push("res/models/tennisball/Tennis Ball-1.obj", &rose);
-    vk::push("res/models/stick/lathi.obj", &city);
+    vk::push("res/models/tennisball/Tennis Ball-1.obj", &dp::ball1);
+    vk::push("res/models/tennisball/Tennis Ball-1.obj", &dp::ball2);
+    vk::push("res/models/stick/lathi.obj", &dp::stick1);
+    vk::push("res/models/stick/lathi.obj", &dp::stick2);
 
     return vk::run();
 
